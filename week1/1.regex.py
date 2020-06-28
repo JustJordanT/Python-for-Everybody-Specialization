0b1c5c0f9c53a1fdaa9d14692e52f46d83bcba39
JustJordanT <jordantay9014@gmail.com>
2020-06-28 01:05:10 -0500

    week 3 of class 3

--- week1/1.regex.py
+++ week1/1.regex.py
@@ -1,9 +1,23 @@
 import re
 
 
-fname = input('Enter File Name: ' )
-if len(fname) < 1 : fname = '42.txt'
-hand = open(fname)
+# fname = input('Enter File Name: ' )
+# if len(fname) < 1 : fname = '42.txt'
+# hand = open(fname)
 
-y = re.findall('[0-9]+', hand)
-print(sum(y))
+count = 0
+hand = open('41.txt')
+line = hand.read()
+x = re.findall('[0-9]+', line)
+for n in x:
+    number = int(n)
+    count = count + number
+print(count)
+    # if len(x) > 0:
+    #     type(x) 
+    #     for y in range(len(x)):
+    #         z = x[y]
+    #         print("".join(z))
+        #
+# y = re.findall('[0-9]+', hand)
+# print(sum(y))
