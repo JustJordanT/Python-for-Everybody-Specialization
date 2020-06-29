ac4b814b17c14173d1707632474b809554e5cccc
JustJordanT <jordantay9014@gmail.com>
2020-06-28 23:31:48 -0500

    Updates working on week 4
    BEAUTIFUL SOUP

--- Using-Python-to-Access-the-Web/week4/HTML-bs4.py
+++ Using-Python-to-Access-the-Web/week4/HTML-bs4.py
@@ -5,17 +5,21 @@
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 import ssl
+import re
 
 # Ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-url = input('Enter: ')
+count = 0
+url = ('http://py4e-data.dr-chuck.net/comments_695397.html')
 html = urllib.request.urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html, 'html.parser')
-
+x = re.findall('[0-9]+', html)
+print(x)
 # Retrieve all of the anchor tags
-tags = soup('a')
-for tag in tags:
-    print(tag.get('href', None))
+for n in x:
+    number = int(n)
+    count = count + number
+print(count)
